52b31bb86a72f090d0878e9843afeb992564f9e4
Robertino <gladdenrobertino@gmail.com>
2023-12-04 13:23:23 +0700

    initial commit

--- setapi.js
+++ setapi.js
@@ -7,7 +7,9 @@ setInterval(() => {
     .then((res) => {
       var field = JSON.stringify(res.feeds[0]);
       var obj = JSON.parse(field);
+      var motionStatus = obj.field2 === '1' ? 'HIGH' : 'LOW'; // Ubah teks sesuai dengan kondisi field2
+      
       document.getElementById("field-1").innerHTML = obj.field1;
-      document.getElementById("field-2").innerHTML = obj.field2;
+      document.getElementById("field-2").innerHTML = motionStatus;
     });
 }, 1000);
